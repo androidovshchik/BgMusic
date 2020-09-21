910ce22ba1e6f20f552158efc4d4b81e794ec365
vlad <vladkalyuzhnyu@gmail.com>
2020-09-21 17:13:57 +0300

    init

--- app/src/main/java/defpackage/bgmusic/MainActivity.kt
+++ app/src/main/java/defpackage/bgmusic/MainActivity.kt
@@ -1,11 +1,72 @@
 package defpackage.bgmusic
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.CookieManager
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.UI
+import org.jetbrains.anko.frameLayout
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.webView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    private val webClient = WebClient()
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(UI {
+            frameLayout {
+                layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
+                webView = webView {
+                    settings.apply {
+                        javaScriptEnabled = true
+                        domStorageEnabled = true
+                        databaseEnabled = true
+                    }
+                    webViewClient = webClient
+                    CookieManager.getInstance().also {
+                        it.setAcceptCookie(true)
+                        it.setAcceptThirdPartyCookies(this, true)
+                    }
+                }.lparams(matchParent, matchParent)
+            }
+        }.view)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        webView.onResume()
+    }
+
+    override fun onPause() {
+        webView.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        CookieManager.getInstance().flush()
+        super.onStop()
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onDestroy() {
+        try {
+            webView.destroy()
+        } catch (ignored: Throwable) {
+        }
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/defpackage/bgmusic/MusicService.kt
@@ -0,0 +1,56 @@
+package defpackage.bgmusic
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+
+@Suppress("MemberVisibilityCanBePrivate")
+class SoundService : Service() {
+
+    private lateinit var exoPlayer: SimpleExoPlayer
+
+    private lateinit var rawSoundFactory: ProgressiveMediaSource.Factory
+
+    private val binder = Binder()
+
+    override fun onCreate() {
+        super.onCreate()
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(applicationContext)
+        rawSoundFactory = ProgressiveMediaSource.Factory(DataSource.Factory {
+            RawResourceDataSource(applicationContext)
+        })
+    }
+
+    override fun onBind(intent: Intent): IBinder? = null
+
+    override fun onUnbind(intent: Intent): Boolean {
+        stopPlay()
+        return true
+    }
+
+    fun playSound(id: Int) {
+        exoPlayer.apply {
+            prepare(
+                rawSoundFactory.createMediaSource(
+                    RawResourceDataSource.buildRawResourceUri(id)
+                )
+            )
+            playWhenReady = true
+        }
+    }
+
+    fun stopPlay() {
+        exoPlayer.playWhenReady = false
+    }
+
+    override fun onDestroy() {
+        stopPlay()
+        exoPlayer.apply {
+            stop()
+            release()
+        }
+        super.onDestroy()
+    }
+}
