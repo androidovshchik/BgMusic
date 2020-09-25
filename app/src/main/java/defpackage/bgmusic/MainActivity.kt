a20f917d26145a540968110a75143207d143218e
vlad <vladkalyuzhnyu@gmail.com>
2020-09-25 07:42:39 +0300

    init

--- app/src/main/java/defpackage/bgmusic/MainActivity.kt
+++ app/src/main/java/defpackage/bgmusic/MainActivity.kt
@@ -1,12 +1,18 @@
 package defpackage.bgmusic
 
 import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.ViewGroup
 import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import defpackage.bgmusic.extension.isLollipopMR1Plus
 import org.jetbrains.anko.UI
 import org.jetbrains.anko.frameLayout
 import org.jetbrains.anko.matchParent
@@ -39,6 +45,28 @@ class MainActivity : AppCompatActivity() {
             }
         }.view)
         webView.loadUrl("https://yandex.ru")
+        requirePermission()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (isLollipopMR1Plus()) {
+            requirePermission()
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP_MR1)
+    private fun requirePermission() {
+        if (!isNotificationServiceEnabled()) {
+            try {
+                val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+            } catch (ignored: Throwable) {
+                // api level < 22
+            }
+        }
     }
 
     override fun onResume() {
@@ -62,6 +90,19 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
+    }
+
+    private fun isNotificationServiceEnabled(): Boolean {
+        val flat = Settings.Secure.getString(contentResolver, "enabled_notification_listeners")
+        if (!flat.isNullOrBlank()) {
+            flat.split(":").forEach {
+                val component = ComponentName.unflattenFromString(it)
+                if (packageName == component?.packageName) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     override fun onDestroy() {
