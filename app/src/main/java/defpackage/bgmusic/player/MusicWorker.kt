80f18f38fe0052373cdb2781ac0187c66b65a86c
vlad <vladkalyuzhnyu@gmail.com>
2020-09-28 13:33:09 +0300

    init

--- app/src/main/java/defpackage/bgmusic/player/MusicWorker.kt
+++ app/src/main/java/defpackage/bgmusic/player/MusicWorker.kt
@@ -1,12 +1,44 @@
 package defpackage.bgmusic.player
 
 import android.content.Context
+import androidx.lifecycle.Observer
+import androidx.lifecycle.observeForeverFreshly
+import androidx.lifecycle.removeFreshObserver
 import androidx.work.*
+import defpackage.bgmusic.playbackChanges
+import java.util.concurrent.TimeUnit
 
-class MusicWorker(context: Context, params: WorkerParameters) : CoroutineWorker(context, params) {
+class MusicWorker(context: Context, params: WorkerParameters) : Worker(context, params),
+    Observer<Boolean> {
 
-    override suspend fun doWork(): Result {
+    private val holder by lazy { ContextHolder(applicationContext) }
+
+    private val player by lazy { MusicPlayer(holder, applicationContext) }
+
+    override fun doWork(): Result {
+        try {
+            player.preparePlaylist(preferences.track, preferences.position)
+            player.startPlay()
+            playbackChanges.observeForeverFreshly(this)
+            Thread.sleep(TimeUnit.MINUTES.toMillis(10))
+        } catch (e: Throwable) {
+            if (!isStopped) {
+                onStopped()
+            }
+        }
         return Result.success()
+    }
+
+    override fun onChanged(hasPause: Boolean) {
+        if (hasPause) {
+            player.startPlay()
+        }
+    }
+
+    override fun onStopped() {
+        holder.saveProgress()
+        playbackChanges.removeFreshObserver(this)
+        player.release()
     }
 
     companion object {
@@ -18,6 +50,12 @@ class MusicWorker(context: Context, params: WorkerParameters) : CoroutineWorker(
                 .build()
             WorkManager.getInstance(context).apply {
                 enqueueUniqueWork(NAME, ExistingWorkPolicy.REPLACE, request)
+            }
+        }
+
+        fun cancel(context: Context) {
+            WorkManager.getInstance(context).apply {
+                cancelUniqueWork(NAME)
             }
         }
     }
